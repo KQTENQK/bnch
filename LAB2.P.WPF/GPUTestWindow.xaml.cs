609229f142aec3954393c6cc9bc5874e65df720b
KQTENQK <xsx3xx@gmail.com>
2021-11-21 18:37:37 +0300

    init

---
+++ LAB2.P.WPF/GPUTestWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace LAB2.P.WPF
+{
+
+    public partial class GPUTestWindow : Window
+    {
+        private SharpGL.OpenGL _gl;
+        private float _xzScreenSize;
+        private Stopwatch _stopwatch = new Stopwatch();
+        private TimeSpan _duration = new TimeSpan(0, 0, 50);
+        private int _score = 0;
+        private float _rotationAngle = 30;
+
+        public event Action<int> Ended;
+
+        public GPUTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnOpenGLDraw(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
+        {
+            _stopwatch.Start();
+
+            if (_stopwatch.Elapsed > _duration)
+            {
+                _stopwatch.Stop();
+                int videoPoints = _score;
+                Ended?.Invoke(videoPoints);
+                Close();
+            }
+
+            _gl = args.OpenGL;
+            _gl.Clear(SharpGL.OpenGL.GL_COLOR_BUFFER_BIT | SharpGL.OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            _gl.LoadIdentity();
+            _gl.Translate(0, 0f, -1f);
+            _gl.Rotate(_rotationAngle, 1f, 1f, 1f);
+
+            for (float xi = (-1 / _xzScreenSize) * 3; xi < (1 / _xzScreenSize) * 3; xi += .6f)
+                for (float yi = -3; yi < 3; yi += .6f)
+                    for (float zi = (-1 / _xzScreenSize) * 3; zi < (1 / _xzScreenSize) * 3; zi += .6f)
+                        DrawPiramid(xi, yi, zi, ref _gl, .1f);
+
+            _rotationAngle += 5;
+
+            _score++;
+        }
+
+        private void DrawPiramid(float x, float y, float z, ref SharpGL.OpenGL gl, float size)
+        {
+            gl.Begin(SharpGL.Enumerations.BeginMode.Polygon);
+
+            // down
+            gl.Color((byte)250, (byte)0, (byte)189);
+            gl.Normal(1.0f, 0.0f, 0.0f);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+
+            gl.End();
+
+            gl.Begin(SharpGL.Enumerations.BeginMode.Polygon);
+
+            //first
+            gl.Color((byte)9, (byte)37, (byte)217);
+            gl.Normal(0.0f, 0.0, -1.0f);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+            gl.Vertex(0, y + 1.0f * size, 0);
+
+            gl.End();
+
+            gl.Begin(SharpGL.Enumerations.BeginMode.Polygon);
+
+            //Second
+            gl.Color((byte)2, (byte)240, (byte)145);
+            gl.Normal(0.0f, -1.0f, 0.0f);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex(0, y + 1.0f * size, 0);
+
+            gl.End();
+
+            gl.Begin(SharpGL.Enumerations.BeginMode.Triangles);
+
+            //Third
+            gl.Color((byte)195, (byte)217, (byte)9);
+            gl.Normal(0.0f, 1.0f, 0.0f);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z - 1.0f * size) * _xzScreenSize);
+            gl.Vertex(0, y + 1.0f * size, 0);
+
+            gl.End();
+
+            gl.Begin(SharpGL.Enumerations.BeginMode.Triangles);
+
+            // Fourth
+            gl.Color((byte)252, (byte)142, (byte)10);
+            gl.Normal(0.0f, 0.0f, 1.0f);
+            gl.Vertex((x - 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+            gl.Vertex((x + 1.0f * size) * _xzScreenSize, 0, (z + 1.0f * size) * _xzScreenSize);
+            gl.Vertex(0, y + 1.0f * size, 0);
+
+            gl.End();
+        }
+
+        private void OnOpenGLInitialized(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
+        {
+            SharpGL.OpenGL gl = args.OpenGL;
+            gl.ClearColor(0.3f, 0.3f, 0.3f, 0.3f);
+        }
+
+        private void OnOpenGLControlResized(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
+        {
+            _xzScreenSize = (float)_glControl.ActualHeight / (float)_glControl.ActualWidth;
+        }
+    }
+}
---
+++ LAB2.P.WPF/MainWindow.xaml.cs
@@ -0,0 +1,245 @@
+﻿using MathNet.Numerics.Interpolation;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+
+namespace LAB2.P.WPF
+{
+    public partial class MainWindow : Window
+    {
+        private DataTable _table = new DataTable();
+        private DataTable _normalizedTable = new DataTable();
+        private string _name = "this";
+        private int _cpuScore = 0;
+        private int _gpuScore = 0;
+        private int _memoryScore = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeDataGridWithDefault();
+
+            CPUTest();
+            GPUTest();
+            MemoryTest();
+
+            _table.Rows.Add(_name, _cpuScore, _gpuScore, _memoryScore, (_cpuScore + _gpuScore + _memoryScore) / 3);
+
+            int maxCPU = 0;
+            int maxGPU = 0;
+            int maxMemory = 0;
+            int maxOverall = 0;
+
+            for (int i = 0; i < _table.Rows.Count - 1; i++)
+            {
+                if (int.Parse(_table.Rows[i][1].ToString()) > maxCPU)
+                    maxCPU = int.Parse(_table.Rows[i][1].ToString());
+
+                if (int.Parse(_table.Rows[i][2].ToString()) > maxGPU)
+                    maxGPU = int.Parse(_table.Rows[i][2].ToString());
+
+                if (int.Parse(_table.Rows[i][3].ToString()) > maxMemory)
+                    maxMemory = int.Parse(_table.Rows[i][3].ToString());
+
+                if (int.Parse(_table.Rows[i][4].ToString()) > maxOverall)
+                    maxOverall = int.Parse(_table.Rows[i][4].ToString());
+            }
+
+            for (int i = 0; i < _table.Rows.Count - 1; i++)
+            {
+                float normalizedCPU = (float)int.Parse(_table.Rows[i][1].ToString()) / (float)maxCPU;
+                float normalizedGPU = (float)int.Parse(_table.Rows[i][2].ToString()) / (float)maxGPU;
+                float normalizedMemory = (float)int.Parse(_table.Rows[i][3].ToString()) / (float)maxMemory;
+                float normalizedOverall = (float)int.Parse(_table.Rows[i][4].ToString()) / (float)maxOverall;
+
+                _normalizedTable.Rows.Add($"PC{i + 1}", normalizedCPU, normalizedGPU, normalizedMemory, normalizedOverall);
+            }
+
+            float normalizedCPUThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][1].ToString()) / (float)maxCPU;
+            float normalizedGPUThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][2].ToString()) / (float)maxGPU;
+            float normalizedMemoryThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][3].ToString()) / (float)maxMemory;
+            float normalizedOverallThis = (float)int.Parse(_table.Rows[_table.Rows.Count - 1][4].ToString()) / (float)maxOverall;
+
+            _normalizedTable.Rows.Add(_name, normalizedCPUThis, normalizedGPUThis, normalizedMemoryThis, normalizedOverallThis);
+        }
+
+        private void InitializeDataGridWithDefault()
+        {
+
+            _table.Columns.Add("Name");
+            _table.Columns.Add("CPU Score");
+            _table.Columns.Add("GPU Score");
+            _table.Columns.Add("Memory Score");
+            _table.Columns.Add("Overall");
+            _dataGrid.ItemsSource = _table.DefaultView;
+
+            _table.Rows.Add("PC1", 165, 196, 179, 180);
+            _table.Rows.Add("PC2", 213, 513, 311, 346);
+            _table.Rows.Add("PC3", 297, 677, 434, 469);
+
+            _normalizedTable.Columns.Add("Name");
+            _normalizedTable.Columns.Add("CPU Score");
+            _normalizedTable.Columns.Add("GPU Score");
+            _normalizedTable.Columns.Add("Memory Score");
+            _normalizedTable.Columns.Add("Overall");
+
+            _dataGridNormalized.ItemsSource = _normalizedTable.DefaultView;
+        }
+
+        private void MemoryTest()
+        {
+            string filePath = @"C:\BenchMark\Test.txt";
+            string directory = @"C:\BenchMark";
+            Directory.CreateDirectory(directory);
+
+            TimeSpan testDuration = new TimeSpan(0, 0, 5);
+            Stopwatch stopwatch = new Stopwatch();
+
+            try
+            {
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    stopwatch.Start();
+                    Random random = new Random();
+                    while (stopwatch.Elapsed < testDuration)
+                    {
+                        streamWriter.WriteLine("fewfkojcjancjiqwjndjwngfrhbneqjdw   qjdnwqkevjndskvjndfkjvnijhfnjiwefndskvmn fkjdvnjkwerfn nbdijvnmewlfewmnvj");
+                        streamWriter.WriteLine("fewfkojcjancjiqwjndjwngfrhbneqjdw   qjdnwqkevjndskvjndfkjvnijhfnjiwefndskvmn fkjdvnjkwerfn nbdijvnmewlfewmnvj");
+                        streamWriter.WriteLine("fewfkojcjancjiqwjndjwngfrhbneqjdw   qjdnwqkevjndskvjndfkjvnijhfnjiwefndskvmn fkjdvnjkwerfn nbdijvnmewlfewmnvj");
+                        streamWriter.WriteLine("fewfkojcjancjiqwjndjwngfrhbneqjdw   qjdnwqkevjndskvjndfkjvnijhfnjiwefndskvmn fkjdvnjkwerfn nbdijvnmewlfewmnvj");
+                        _memoryScore++;
+                    }
+                }
+
+                stopwatch.Restart();
+
+                while (stopwatch.Elapsed < testDuration)
+                {
+                    using (StreamReader streamReader = new StreamReader(filePath))
+                    {
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.Append(streamReader.ReadToEnd());
+                        _memoryScore++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            _memoryScore /= 10000;
+
+            File.Delete(filePath);
+            Directory.Delete(directory);
+        }
+
+        private void GPUTest()
+        {
+            var videoTest = new GPUTestWindow();
+            videoTest.Ended += OnGpuTestEnded;
+            videoTest.ShowDialog();
+        }
+
+        private void OnGpuTestEnded(int score)
+        {
+            _gpuScore = score;
+        }
+
+        private void CPUTest()
+        {
+            Random rand = new Random();
+
+            const double e = 0.001;
+            TimeSpan testTime = new TimeSpan(0, 0, 50);
+
+            Stopwatch testStopwatch = new Stopwatch();
+
+            testStopwatch.Start();
+
+            while (testStopwatch.Elapsed < testTime)
+            {
+                List<double> values = new List<double>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    values.AddRange(SolveDerivative(GenerateRandomPoints(10, rand), GenerateRandomPoints(10, rand), e));
+                }
+
+                ShellSort(values);
+
+                _cpuScore++;
+            }
+        }
+
+        private List<double> SolveDerivative(double[] xControlPoints, double[] yControlPoints, double e)
+        {
+            Vector<double> xPoints = DenseVector.OfArray(xControlPoints);
+            Vector<double> yPoints = DenseVector.OfArray(yControlPoints);
+            CubicSpline cubicSpline = CubicSpline.InterpolateNatural(xPoints, yPoints);
+
+            List<double> derivativesValues = new List<double>();
+
+            for (double t = 0; t < xControlPoints[xControlPoints.Length - 1]; t += e)
+            {
+                derivativesValues.Add(cubicSpline.Differentiate(t));
+            }
+
+            return derivativesValues;
+        }
+
+        private double[] GenerateRandomPoints(int count, Random random)
+        {
+            double[] points = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                points[i] = random.Next(-100, 100) + random.Next(0, 4) * random.NextDouble();
+            }
+
+            System.Threading.Thread.Sleep(1);
+
+            ShellSort(points);
+
+            return points;
+        }
+
+        private void ShellSort(double[] array)
+        {
+            for (int i = array.Length / 2; i > 0; i = i / 2)
+            {
+                for (int j = i; j < array.Length; ++j)
+                {
+                    for (int k = j - i; k >= 0 && array[k] > array[k + i]; k = k - i)
+                    {
+                        double temp = array[k];
+                        array[k] = array[k + i];
+                        array[k + i] = temp;
+                    }
+                }
+            }
+        }
+
+        private void ShellSort(List<double> array)
+        {
+            for (int i = array.Count / 2; i > 0; i = i / 2)
+            {
+                for (int j = i; j < array.Count; ++j)
+                {
+                    for (int k = j - i; k >= 0 && array[k] > array[k + i]; k = k - i)
+                    {
+                        double temp = array[k];
+                        array[k] = array[k + i];
+                        array[k + i] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
